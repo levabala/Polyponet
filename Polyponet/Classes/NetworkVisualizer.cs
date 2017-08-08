f92e32d1561738cb93e72a0afcfe57c8b4430511
Levabala <levabala@ya.ru>
2017-08-08 19:24:02 +0300

    v0.7

---
+++ Polyponet/Classes/NetworkVisualizer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Polyponet.Classes
+{
+    class NetworkVisualizer : FrameworkElement
+    {
+        public double NODE_RADIUS, ELASTICITY;
+        public int RADIUSES_BETWEEN;
+
+        private Network network;         
+        private Dictionary<byte[], Node> visual
+
+        public Network Network
+        {
+            get
+            {
+                return network;
+            }
+            set
+            {
+                network = value;
+
+            }
+        }
+
+        public NetworkVisualizer()
+        {
+
+        }
+
+        protected override void OnRender(DrawingContext context)
+        {
+            foreach (Node n in network.nodes)
+        }        
+
+        private double getPullRate(double distance, double interaction, double radius, double elasticity)
+        {
+            double f1 = -1 / Math.Pow(distance + interaction - radius, 7);
+            double f2 = 1 / Math.Pow(distance + interaction - radius, 13);
+            double pullRate = (1 / elasticity) * (f1 + f2);
+
+            return pullRate;
+        }
+
+        private class NodeVisual
+        {
+            public double x, y, radius;
+            public Pen pen;
+
+            public NodeVisual(double x, double y, double radius)
+            {
+                this.x = x;
+                this.y = y;
+                this.radius = radius;
+            }
+
+            public void render(DrawingContext context)
+            {
+                context.DrawEllipse(Brushes.Transparent, pen, new Point(x, y), 5, 5);
+            }
+        }
+    }
+}
---
+++ Polyponet/Classes/Vector.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+using System.Windows;
+
+namespace Polyponet.Classes
+{
+    class Vector
+    {
+        public double length, alpha;
+        public Point startPoint, endPoint;
+
+        public Vector()
+        {
+            length = 0.001f;
+            alpha = 0f;
+            startPoint = new Point(0, 0);
+            endPoint = new Point(1, 1);
+        }
+
+        public Vector(double length, double alpha, Point startPoint)
+        {
+            this.length = length;
+            this.alpha = alpha;
+            this.startPoint = startPoint;
+            endPoint = getEndPoint();
+        }
+
+        public Vector(Point startPoint, Point endPoint)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            length = getLength();
+            alpha = getAlpha();
+        }
+
+        public Point getEndPoint()
+        {
+            Point pf = new Point(this.length * Math.Cos(alpha) + startPoint.X, length * Math.Sin(alpha) + startPoint.Y);
+            return pf;
+        }
+
+        public void changeDirection(double alpha)
+        {
+            this.alpha = alpha;
+            getEndPoint();
+        }
+
+        public double getAlpha()
+        {
+            double dx = endPoint.X - startPoint.X;
+            double dy = endPoint.Y - startPoint.Y;
+            if (dx == 0) return 0;
+            double angle = Math.Atan(dy / dx);
+            if ((dx < 0 && dy < 0) || (dx < 0 && dy >= 0)) angle = angle - Math.PI;            
+
+            return angle;
+        }
+
+        public double getAlpha(Point endPoint, double length)
+        {
+            double dx = Math.Abs(this.endPoint.X - startPoint.X);
+            double dy = Math.Abs(this.endPoint.Y - startPoint.Y);
+            double angle = Math.Atan(dx / dy);
+            if (dx < 0) angle = angle - 3 * Math.PI;
+            return angle;
+        }
+
+        public double getLength()
+        {
+            double lengthX = Math.Abs(endPoint.X - startPoint.X);
+            double lengthY = Math.Abs(endPoint.Y - startPoint.Y);
+            return (double)Math.Sqrt(lengthX * lengthX + lengthY * lengthY);
+        }
+
+        public double getLength(Point endPoint, Point startPoint)
+        {
+            double lengthX = Math.Abs(endPoint.X - startPoint.X);
+            double lengthY = Math.Abs(endPoint.Y - startPoint.Y);
+            return (double)Math.Sqrt(lengthX * lengthX + lengthY * lengthY);
+        }
+
+        public Vector sum(Vector a, Vector b)
+        {
+            Vector sumV = new Vector();
+            sumV.startPoint = a.startPoint;
+            sumV.endPoint = b.endPoint;
+            sumV.length = getLength(sumV.endPoint, sumV.startPoint);
+            sumV.alpha = getAlpha(sumV.endPoint, sumV.length);
+            return sumV;
+        }
+    }
+}
